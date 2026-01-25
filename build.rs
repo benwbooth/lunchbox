7bf9525d92b1ae255a16a27b8c0ba0f287d9a8af
Ben Booth <benwbooth@gmail.com>
2026-01-24 20:43:23 -0800

    Add proper timezone abbreviations (PST/EST) to build timestamps

--- build.rs
+++ build.rs
@@ -1,4 +1,5 @@
 use std::process::Command;
+use chrono::TimeZone;
 
 fn main() {
     // Generate content hash from git
@@ -9,8 +10,14 @@ fn main() {
         .unwrap_or_else(|_| "unknown".to_string());
     println!("cargo:rustc-env=BUILD_HASH={}", hash);
 
-    // Generate build timestamp (local timezone, space-separated)
-    let timestamp = chrono::Local::now().format("%Y-%m-%d %H:%M:%S %Z").to_string();
+    // Generate build timestamp with timezone abbreviation (e.g., PST, EST)
+    let now = chrono::Local::now();
+    let tz_abbrev = iana_time_zone::get_timezone()
+        .ok()
+        .and_then(|tz_name| tz_name.parse::<chrono_tz::Tz>().ok())
+        .and_then(|tz| Some(tz.from_utc_datetime(&now.naive_utc()).format("%Z").to_string()))
+        .unwrap_or_default();
+    let timestamp = format!("{} {}", now.format("%Y-%m-%d %H:%M:%S"), tz_abbrev);
     println!("cargo:rustc-env=BUILD_TIMESTAMP={}", timestamp);
 
     // Rerun if git state changes
