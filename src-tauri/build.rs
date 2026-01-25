147464d30f7f7aa30613167e3e1f54d5ce3dd3c8
Ben Booth <benwbooth@gmail.com>
2026-01-24 20:39:41 -0800

    Add timezone abbreviation to build timestamps

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -12,7 +12,7 @@ fn main() {
     println!("cargo:rustc-env=BUILD_HASH={}", hash);
 
     // Generate build timestamp (local timezone, space-separated)
-    let timestamp = chrono::Local::now().format("%Y-%m-%d %H:%M:%S").to_string();
+    let timestamp = chrono::Local::now().format("%Y-%m-%d %H:%M:%S %Z").to_string();
     println!("cargo:rustc-env=BUILD_TIMESTAMP={}", timestamp);
 
     // Rerun if git state changes
